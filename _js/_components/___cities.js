c5ed4083a48982c7db594b0ce162b672f774a8a0
chasvales <valeschas@gmail.com>
2016-06-06 15:28:03 -0400

    removed unused items

--- _js/_components/___cities.js
+++ _js/_components/___cities.js
@@ -3,37 +3,27 @@ var cities_nav = {
 		0 : {
 			city : 'London',
 			country : 'United Kingdom',
-			conutryabv : 'UK',
-			image : 'Bravo Music',
-			timezone : 'Mind over Matter'
+			conutryabv : 'UK'
 		},
 		1 : {
 			city : 'Amsterdam',
 			country : 'Netherlands',
-			conutryabv : 'NL',
-			image : 'Bravo Music',
-			timezone : 'Mind over Matter'
+			conutryabv : 'NL'
 		},
 		2 : {
 			city : 'Prague',
 			country : 'Czech Republic',
-			conutryabv : 'CZ',
-			image : 'Bravo Music',
-			timezone : 'Mind over Matter'
+			conutryabv : 'CZ'
 		},
 		3 : {
 			city : 'Paris',
 			country : 'France',
-			conutryabv : 'FR',
-			image : 'Bravo Music',
-			timezone : 'Mind over Matter'
+			conutryabv : 'FR'
 		},
 		4 : {
 			city : 'Barcelona',
 			country : 'Spain',
-			conutryabv : 'ES',
-			image : 'Bravo Music',
-			timezone : 'Mind over Matter'
+			conutryabv : 'ES'
 		}
 	}
 }
